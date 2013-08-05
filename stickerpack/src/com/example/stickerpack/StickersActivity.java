a21fc1e416a79ae4e3e14705703dcab9e68de52c
n8fr8 <nathan@freitas.net>
2013-08-05 16:42:15 -0400

    fixing stickerpack project to add default activity

---
+++ stickerpack/src/com/example/stickerpack/StickersActivity.java
@@ -0,0 +1,10 @@
+package com.example.stickerpack;
+import android.app.Activity;
+
+
+/*
+ * this is an empty placeholder activity for sticker plugins
+ */
+public class StickersActivity extends Activity {
+
+}
